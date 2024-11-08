192d89c652afb02a45aa110ebb519264d64fd584
曾源喆 <adam880614@gmail.com>
2024-11-09 01:11:18 +0800

    100 85.11

---
+++ 26.remove-duplicates-from-sorted-array.c
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=26 lang=c
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+int removeDuplicates(int* nums, int numsSize) {
+    if ( numsSize == 0) { return 0; }
+
+    int k = 0;
+    for ( int i = 1; i < numsSize; i++) {
+        if ( nums[i] != nums[k] ) {
+            k++;
+            nums[k] = nums[i];
+        }
+    }
+    return k + 1;
+}
+// @lc code=end
+
