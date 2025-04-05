c903d6505a2c490f35e8bdb63dac36bcd541502c
TsengYuanChe <adam880614@gmail.com>
2025-04-05 17:29:45 +0800

    Addition

---
+++ 35.search-insert-position.c
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=35 lang=c
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+int searchInsert(int* nums, int numsSize, int target) {
+    
+}
+// @lc code=end
+
